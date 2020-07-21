ce3c9cd879c5cf58c9010c5a275e743bf073a388
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-07-21 14:30:42 +0530

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname        = "https://app.terraform.io"   #For SaaS use "app.terraform.io"
     organization    = "surya_accenture2"   #Your Org, top-left corner of the TFE UI
     workspaces {
-      name = "ws-aws-ex1"  #Workspace to connect to (lives within the Org)
+      name = "workspace-test"  #Workspace to connect to (lives within the Org)
     }
   }
 }
